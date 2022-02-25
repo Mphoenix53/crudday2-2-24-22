92729a40dd783f5fe1103564400f7df085654527
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-02-24 17:23:00 -0700

    have the API functionrendering data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const getUsers = ()=>{
+    axios
+    .get("https://reqres.in/api/users?page=2")
+    .then((res) => {
+      console.log('res:', res)
+      console.log('res.data', res.data)
+      console.log('res.data.data', res.data.data)
+      setUsers(res.data.data)
+    })
+    .catch((err) => {
+      console.log('err:', err)
+    });
+  
+  };
   return (
     <div className="App">
-      
+      <h1>53</h1>
+      <button onClick={getUsers}>Get info</button>
+      <div>{JSON.stringify(users)}</div>
     </div>
   );
 }
